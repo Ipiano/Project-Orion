4218caf17c2e2fe82673e6007721b3a72ab8e71e
Andrew Stelter <andrew.d.stelter@gmail.com>
2016-12-17 18:22:24 -0600

    no message

--- Betelgeuse/deploy.bat
+++ Betelgeuse/deploy.bat
@@ -1,5 +1,7 @@
 del /Q /S deploy\*
+mkdir deploy
 copy /B /Y release\release\Betelgeuse.exe deploy\Betelgeuse.exe
 cd deploy
 windeployqt Betelgeuse.exe
-7za a Orion.zip *
+7za a Betelgeuse.zip *
+move /Y Betelgeuse.zip ..\Betelgeuse.zip
